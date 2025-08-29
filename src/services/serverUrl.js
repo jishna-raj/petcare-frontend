50bccd7796cb33f0693f6a1776dff8b946da3640
jishna-raj <jishnaraj01042000@gmail.com>
2025-08-29 10:03:36 +0530

    frontend

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -4,4 +4,4 @@
 
 // export const serverUrl ='http://localhost:4000'
 
-export const serverUrl ='https://petcare-server-27mb.onrender.com'
+export const serverUrl ='https://petcare-server-2.onrender.com'
